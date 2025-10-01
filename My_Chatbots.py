ba1eeaed581de9fba073d3674f545694d8434fc0
weiweiwe-1 <3066504769@qq.com>
2025-10-01 19:28:12 +0800

    Update My_Chatbots.py

--- My_Chatbots.py
+++ My_Chatbots.py
@@ -15,13 +15,13 @@ with st.sidebar:
     st.markdown("[获取Tongyi账号的API KEY](https://bailian.console.aliyun.com/?spm=5176.29597918.J_SEsSjsNv72yRuRFS2VknO.2.6a2e7b087b88Ea&tab=model#/api-key)")
 
 # 3. 主界面主标题
-st.title('通义聊天机器人')
+st.title('李子涵聊天机器人')
 
 # 4. 会话保持: 用于存储会话记录.
 if 'memory' not in st.session_state:
     # 初始化会话记录, memory: 会话记录, messages: 会话记录中的消息列表
     st.session_state['memory'] = ConversationBufferMemory()
-    st.session_state['messages'] = [{'role':'ai', 'content':'你好, 我是通义聊天机器人, 有什么可以帮助你的吗?'}]
+    st.session_state['messages'] = [{'role':'ai', 'content':'主人您好！我是李子涵，我从小就牛逼，3岁就会偷看寡妇洗澡，6岁偷了全村人的裤衩，15岁多次嫖娼进入少管所，16岁到22岁根据亲身经历写出《舔狗秘诀》，有什么都可以问我，也可以和我聊天，我什么都知道，包括如何让你从1cm到5cm'}]
 
 # 5. 编写1个循环结构, 用于打印会话记录(中的消息列表)
 for message in st.session_state.messages:
@@ -30,7 +30,7 @@ for message in st.session_state.messages:
         st.markdown(message['content'])         #打印内容
 
 # 6. 创建1个聊天窗口
-prompt = st.chat_input("请输入您要咨询的问题:")
+prompt = st.chat_input("主人想问我点什么吗？还是想和我聊点什么？")
 
 def get_response(prompt,memory,api_key):
     """
@@ -54,7 +54,6 @@ if prompt:  # 如果文本框有数据, 继续往下执行.
     # st.chat_message('user').markdown(prompt)
 
     # 如果没有API KEY, 直接返回提示.
-    st.write(api_key)
     if not api_key:
         st.warning('请输入Tongyi的API KEY!')
         st.stop()
@@ -64,13 +63,14 @@ if prompt:  # 如果文本框有数据, 继续往下执行.
     
     # 9. 向utils工具类发起请求, 返回响应.
     # 显示一个等待框.
-    with st.spinner('AI小助手正在思考中...'):
+    with st.spinner('我刚刚吹牛逼呢，其实我是弱智，等我扣脚想一下...'):
         content = get_response(prompt, st.session_state['memory'], api_key)
 
     # 10. 把AI的回复信息, 添加到会话记录中.
     st.session_state['messages'].append({'role':'ai', 'content':content})
     # 11. 把AI的回复信息, 显示在主窗体中.
     st.chat_message('ai').markdown(content)
+
 
 
 
